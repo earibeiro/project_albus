a17a73420af2aa486ca1b82c860df9afe0b52906
Eduardo Albuquerque Ribeiro <eduardoribeiro.advg@gmail.com>
2024-09-12 21:56:59 -0300

    entidades phone e phone_test estão funcionando

--- lib/dominio/phone.dart
+++ lib/dominio/phone.dart
@@ -1,18 +1,12 @@
 class Phone {
-  String phone;
+  final String number;
 
-  Phone(this.phone) {
-    if (phone.isEmpty) {
-      throw Exception('Telefone não pode ser vazio!');
+  Phone(this.number) {
+    if (number.isEmpty) {
+      throw ArgumentError('O telefone não pode estar vazio.');
     }
-    if (!_isValidPhone(phone)) {
-      throw Exception('Número de telefone inválido');
+    if (!RegExp(r'^\d{10,11}$').hasMatch(number)) {
+      throw ArgumentError('O telefone deve conter apenas números e ter 10 ou 11 dígitos.');
     }
-  }
-
-  bool _isValidPhone(String phone) {
-    // Expressão regular para validar o telefone com DDD
-    final phoneRegex = RegExp(r'^\(\d{2}\) \d{4,5}-\d{4}$');
-    return phoneRegex.hasMatch(phone);
   }
 }
