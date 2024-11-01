63408857007da87e0704755180fd6f8ce7ed8fce
Eduardo Albuquerque Ribeiro <eduardoribeiro.advg@gmail.com>
2024-10-31 21:27:00 -0300

    Atualização do diário de aula com o escopo do projeto

--- lib/app/application/my_app.dart
+++ lib/app/application/my_app.dart
@@ -6,25 +6,36 @@ import 'package:albus/app/widget/students.dart';
 import 'package:albus/app/widget/formstudents.dart';
 import 'package:albus/app/widget/teachersoptions.dart';
 import 'package:albus/app/widget/formteachers.dart';
-
+import 'package:albus/app/widget/descriptionteacher.dart';
 import 'package:albus/app/widget/teachers.dart';
+import 'package:albus/app/widget/course.dart';
+import 'package:albus/app/widget/courseoptions.dart';
+import 'package:albus/app/widget/formcourse.dart';
+import 'package:albus/app/widget/descriptioncourse.dart';
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Aulas',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.cyan),
-      routes:{
-        Routes.home : (context) => SelectionScreen(),
-        Routes.studentsOptions : (context) => StudentsOptions(),
-        Routes.students : (context) => Students(),
-        Routes.formStudents : (context) => FormStudents(),
-        Routes.teachers : (context) => Teachers(),
-        Routes.formTeacher : (context) => classFormTeachers(),
-        Routes.teachersOptions : (context) => TeachersOptions(),
-      },);
+      routes: {
+        Routes.home: (context) => SelectionScreen(),
+        Routes.studentsOptions: (context) => StudentsOptions(),
+        Routes.students: (context) => Students(),
+        Routes.formStudents: (context) => FormStudents(),
+        Routes.teachers: (context) => Teachers(),
+        Routes.formTeacher: (context) => FormTeachers(),
+        Routes.teachersOptions: (context) => TeachersOptions(),
+        Routes.course: (context) => CoursePage(),
+        Routes.courseOptions: (context) => CourseOptions(),
+        Routes.formCourse: (context) => FormCourse(),
+        Routes.descriptionCourse: (context) => DescriptionCourse(course: ModalRoute.of(context)!.settings.arguments),
+        Routes.descriptionTeacher: (context) => DescriptionTeacher(teacher: ModalRoute.of(context)!.settings.arguments),
+      },
+    );
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,17 @@
-class Routes{
+import 'package:flutter/material.dart';
+import 'package:albus/app/widget/studentsoptions.dart';
+import 'package:albus/app/widget/teachersoptions.dart';
+import 'package:albus/app/widget/courseoptions.dart';
+import 'package:albus/app/widget/formcourse.dart';
+import 'package:albus/app/widget/descriptioncourse.dart';
+import 'package:albus/app/widget/selectionscreen.dart';
+import 'package:albus/app/widget/students.dart';
+import 'package:albus/app/widget/formstudents.dart';
+import 'package:albus/app/widget/teachers.dart';
+import 'package:albus/app/widget/formteachers.dart';
+import 'package:albus/app/widget/descriptionteacher.dart';
+
+class Routes {
   static const String home = '/';
   static const String teachersOptions = '/teachersOptions';
   static const String teachers = '/teachers';
